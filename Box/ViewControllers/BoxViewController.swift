11dd98596da09f17ba042d38196e0a84c802a271
Marat <Shagiakhmetov2499@gmail.com>
2023-07-13 23:18:17 +0300

    Subviews will add when user rotate screen. If screen of device is landscape then will add two new views. Also new views will be deleted if screen of device is portrait.

--- Box/ViewControllers/BoxViewController.swift
+++ Box/ViewControllers/BoxViewController.swift
@@ -28,11 +28,28 @@ class BoxViewController: UIViewController {
         return view
     }()
     
+    private var redLeading: NSLayoutConstraint!
+    private var greenLeading: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupDesign()
         setupSubviews(subviews: redView, greenView)
         setupConstraints()
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        if UIDevice.current.orientation.isLandscape {
+            redLeading.constant = view.frame.height / 2
+            greenLeading.constant = view.frame.height / 2
+            setupSubviews(subviews: blueView, yellowView)
+            setupConstraintsForAddSubviews()
+        } else {
+            redLeading.constant = 20
+            greenLeading.constant = 20
+            removeSubviews(subviews: blueView, yellowView)
+        }
     }
     
     private func setupDesign() {
@@ -44,39 +61,48 @@ class BoxViewController: UIViewController {
             view.addSubview(subview)
         }
     }
+    
+    private func removeSubviews(subviews: UIView...) {
+        subviews.forEach { subview in
+            subview.removeFromSuperview()
+        }
+    }
 }
 // MARK: - Constraints
 extension BoxViewController {
     private func setupConstraints() {
+        redLeading = redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        greenLeading = greenView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        
         NSLayoutConstraint.activate([
             redView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            redLeading,
             redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             redView.heightAnchor.constraint(equalTo: greenView.heightAnchor)
         ])
         
         NSLayoutConstraint.activate([
             greenView.topAnchor.constraint(equalTo: redView.bottomAnchor, constant: 20),
-            greenView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            greenLeading,
             greenView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             greenView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20)
         ])
-        /*
+    }
+    
+    private func setupConstraintsForAddSubviews() {
         NSLayoutConstraint.activate([
             blueView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
-            blueView.trailingAnchor.constraint(equalTo: redView.leadingAnchor, constant: -40),
-            blueView.heightAnchor.constraint(equalTo: redView.heightAnchor),
-            blueView.widthAnchor.constraint(equalTo: redView.widthAnchor)
+            blueView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            blueView.trailingAnchor.constraint(equalTo: redView.leadingAnchor, constant: -20),
+            blueView.bottomAnchor.constraint(equalTo: yellowView.topAnchor, constant: -20),
+            blueView.heightAnchor.constraint(equalTo: redView.heightAnchor)
         ])
         
         NSLayoutConstraint.activate([
-            yellowView.topAnchor.constraint(equalTo: blueView.bottomAnchor, constant: 20),
-            yellowView.trailingAnchor.constraint(equalTo: greenView.leadingAnchor, constant: -40),
-            yellowView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
-            yellowView.heightAnchor.constraint(equalTo: greenView.heightAnchor),
-            yellowView.widthAnchor.constraint(equalTo: greenView.widthAnchor)
+            yellowView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            yellowView.trailingAnchor.constraint(equalTo: greenView.leadingAnchor, constant: -20),
+            yellowView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20)
         ])
-         */
     }
 }
 // MARK: - Subviews
